104bcb84299fcfebebdffd1aca74b0258f03283b
Anmol Mann <mann.anmol15@gmail.com>
2019-05-14 11:40:35 -0700

    Update bash file

--- EmotioNet_job.sh
+++ EmotioNet_job.sh
@@ -8,4 +8,4 @@
 #SBATCH --cpus-per-task=24
 #SBATCH --time=0-30:00            # time (DD-HH:MM)
 
-python solution.py --mode train --log_dir ./logs_EmotioNet --save_dir ./save_EmotioNet --data_dir EmotioNet --resize True
+python main.py --mode train --log_dir ./logs_EmotioNet --save_dir ./save_EmotioNet --data_dir EmotioNet --resize True
